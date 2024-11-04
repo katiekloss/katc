5b14e37393905fe5f65a6a306829223b88a96348
Katie Kloss <kloss.katherine@gmail.com>
2024-11-04 18:11:28 -0500

    planespotter: don't log debug lines

--- planespotter.py
+++ planespotter.py
@@ -12,7 +12,7 @@ callsigns = dict()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 console = logging.StreamHandler()
-console.setLevel(logging.DEBUG)
+console.setLevel(logging.INFO)
 console.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
 logger.addHandler(console)
 
