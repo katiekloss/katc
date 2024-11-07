0feb0918761422c407842c97b97dd557d3f73af0
Katie Kloss <kloss.katherine@gmail.com>
2024-11-07 18:56:26 -0500

    mode_s_router: use CLI args.

--- mode_s_router.py
+++ mode_s_router.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import aio_pika
 import asyncio
+import argparse
 import os
 import pyModeS as pms
 import daemon
@@ -9,14 +10,9 @@ import sys
 from pidlockfile import PIDLockFile
 from setproctitle import setproctitle
 
-async def main():
-    try:
-        conn_string = os.environ["RABBIT_CONNECTION_STRING"]
-    except KeyError:
-        print("I need a Rabbit connection string in RABBIT_CONNECTION_STRING")
-        return
 
-    rabbit = await aio_pika.connect_robust(conn_string)
+async def main(args):
+    rabbit = await aio_pika.connect_robust(args.rabbit)
 
     channel = await rabbit.channel()
     mode_s_exchange = await channel.declare_exchange("mode_s_by_downlink",
@@ -73,10 +69,22 @@ async def route(message, mode_s_exchange, adsb_exchange):
         await adsb_exchange.publish(routed_message, f"icao.{icao}.typecode.{tc}")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("mode_s_router.py /var/run/mode_s_router.pid")
-        sys.exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--daemon", action="store_true")
+    parser.add_argument("-p", "--pidfile")
+    parser.add_argument("-r", "--rabbit", required=True)
+    args = parser.parse_args()
 
-    with daemon.DaemonContext(pidfile = PIDLockFile(sys.argv[1])):
-        setproctitle("yetanother1090monitor: mode_s_router")
-        asyncio.run(main())
+    if args.daemon:
+        if args.pidfile is None or len(args.pidfile) == 0:
+            log.error("-p/--pidfile is required when --daemon is present")
+            sys.exit(1)
+
+        with daemon.DaemonContext(pidfile=PIDLockFile(args.pidfile, timeout=2.0)):
+            log.info("Starting")
+            setproctitle("yetanother1090monitor: mode_s_router")
+            asyncio.run(main(args))
+    else:
+        log.info("Starting")
+        asyncio.run(main(args))
+
