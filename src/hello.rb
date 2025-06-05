467ea150687c10b478eefbedbbc1a0a75b02a0d1
Katie Kloss <kloss.katherine@gmail.com>
2025-06-05 06:42:12 -0400

    feat: use ADSB::Message.parse instead

--- src/hello.rb
+++ src/hello.rb
@@ -4,11 +4,15 @@ require 'adsb'
 s = TCPSocket.new ENV['DUMP1090_HOST'], 30002
 
 while line = s.gets
+  line = line[1..-3]
   begin
-    msg = ADSB::Message.new(line[1..-1])
-    puts msg.address
-  rescue NameError
-    puts "Unknown type code in #{line}"
+    msg = ADSB::Message.parse(line)
+    next unless msg.is_a?(ADSB::Messages::Base)
+
+    puts "#{msg.type_code.to_s.rjust(2)} #{msg.address} #{line}"
+  rescue ArgumentError => e
+    puts "Unknown parse error in #{line}"
+    raise e
   end
 end
 
