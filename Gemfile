00120c8daa9330f4f838c9b4bf5174959edcb599
Katie Kloss <kloss.katherine@gmail.com>
2025-06-09 06:23:54 -0400

    show human readable message summaries

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'ruby-adsb', path: 'vendor/ruby-adsb', require: 'adsb'
+gem "minitest", "~> 5.25"
--- src/hello.rb
+++ src/hello.rb
@@ -9,7 +9,18 @@ while line = s.gets
     msg = ADSB::Message.parse(line)
     next unless msg.is_a?(ADSB::Messages::Base)
 
-    puts "#{msg.type_code.to_s.rjust(2)} #{msg.address} #{line}"
+    human = case
+    when msg.is_a?(ADSB::Messages::Identification)
+      "ident #{msg.identification}"
+    when msg.is_a?(ADSB::Messages::Position)
+      "position #{msg.latitude} #{msg.longitude}"
+    when msg.is_a?(ADSB::Messages::Velocity)
+      "heading #{msg.heading} velocity #{msg.velocity}"
+    else
+      ""
+    end
+
+    puts "#{msg.type_code.to_s.rjust(2)} #{msg.address} #{line} #{human}"
   rescue ArgumentError => e
     puts "Unknown parse error in #{line}"
     raise e
