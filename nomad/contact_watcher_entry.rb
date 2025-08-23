88c9fe2c8648dacf0d293f6b92b7bcc104df9d50
Katie Kloss <kloss.katherine@gmail.com>
2025-08-23 09:58:23 -0400

    puts debugging

--- nomad/contact_watcher_entry.rb
+++ nomad/contact_watcher_entry.rb
@@ -13,6 +13,7 @@ q.bind(xch, routing_key: 'contact_started')
 http = NetX::HTTPUnix.new("unix://#{ENV['NOMAD_SECRETS_DIR']}/api.sock")
 begin
   q.subscribe(block: true) do |_info, _properties, body|
+    puts "Starting job for #{body}"
     req = Net::HTTP::Post.new(URI('/v1/job/katc-watcher/dispatch'), {}.to_json, initheader = {'Content-Type': 'application/json'})
     resp = http.request(req)
     puts resp.body
